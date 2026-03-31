a7f311cc21a93ff6a7696b95b36c694054355b2c
hasol11 <kangji09879@sookmyung.ac.kr>
2026-03-31 21:09:46 +0900

    Feat: 로그아웃 기능 추가

--- week04/accounts/views.py
+++ week04/accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from .forms import SignUpForm
-from django.contrib.auth import authenticate, login as auth_login
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
 
 
 def signup(request):
@@ -26,3 +26,13 @@ def login(request):
             return redirect("main")
 
     return render(request, "login.html")
+
+
+def main(request):
+    return render(request, "main.html")
+
+
+def logout(request):
+    if request.user.is_authenticated:
+        auth_logout(request)
+        return redirect("login")
