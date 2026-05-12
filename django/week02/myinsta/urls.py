62f85106e383eb9cb6235ab79b86746fff70b4b7
hasol11 <kangji09879@sookmyung.ac.kr>
2026-05-12 19:51:40 +0900

    Feat: FBV 작성

--- django/week02/myinsta/urls.py
+++ django/week02/myinsta/urls.py
@@ -19,7 +19,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from posts.views import url_view, url_parameter_view, function_view
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("url/", url_view),
+    path("url/<str:username>/", url_parameter_view),
+    path("fbv/", function_view),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
