cc0ca2e7b40aaf915c06a1351940e94a32640faa
hasol11 <kangji09879@sookmyung.ac.kr>
2026-05-15 16:52:27 +0900

    Feat: DetailView 방식으로 CBV 변경

--- django/week02_hw/posts/views.py
+++ django/week02_hw/posts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Post
 from django.views import View
+from django.views.generic import DetailView
 
 
 # Create your views here.
@@ -9,8 +10,15 @@ def post_list(request):
     context = {"view_type": "Function Based View", "posts": posts}
     return render(request, "post_list.html", context)
 
-class post_detail(View):
-    def get(self, request, id):
-        post = Post.objects.get(id=id)
-        context={"view_type": "Class Based View", "post":post}
-        return render(request, "post_detail.html", context)
+# View 방식
+# class post_detail(View):
+#     def get(self, request, id):
+#         post = Post.objects.get(id=id)
+#         context={"view_type": "Class Based View", "post":post}
+#         return render(request, "post_detail.html", context)
+
+#DetailView 방식
+class post_detail(DetailView):
+    model = Post
+    template_name = "post_detail.html"
+    context_object_name="post"
