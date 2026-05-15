4846bfad010aedc7f1ec04eef0494cc4df7bed0d
hasol11 <kangji09879@sookmyung.ac.kr>
2026-05-15 13:21:19 +0900

    Feat: FBV 뷰 및 템플릿 작성

--- django/week02_hw/posts/views.py
+++ django/week02_hw/posts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Post
+
 
 # Create your views here.
+def post_list(request):
+    posts = Post.objects.all()
+    context = {"view_type": "Function Based View", "posts": posts}
+    return render(request, "post_list.html", context)
