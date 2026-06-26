2d31f5e0c865e29895cddc9902d1027b47112581
hasol11 <kangji09879@sookmyung.ac.kr>
2026-06-26 16:40:07 +0900

    Feat: 비밀번호 해싱

--- django/week04_hw/users/serializers.py
+++ django/week04_hw/users/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework.serializers import ModelSerializer
 from django.contrib.auth.models import User
-from django.contrib.auth.password_validation import validate_password
 
 class UserSerializer(ModelSerializer):
     class Meta:
         model=User 
         fields=['id','username', 'email','password']
         extra_kwargs={'password':{'write_only':True}}
+        
+    def create(self, validated_data):
+        password=validated_data.pop('password')
+        user=User(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
