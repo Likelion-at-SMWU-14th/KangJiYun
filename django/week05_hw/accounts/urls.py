759107c3aa8e00badf12f1d6d78f69059a3bde9c
hasol11 <kangji09879@sookmyung.ac.kr>
2026-07-05 14:57:46 +0900

    Feat: 회원가입 구현

---
+++ django/week05_hw/accounts/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from rest_framework import routers
+from .views import UserModelViewSet
+
+app_name = "accounts"
+
+router_accounts = routers.DefaultRouter()
+router_accounts.register("", UserModelViewSet)
+
+urlpatterns = [
+    path("", include(router_accounts.urls)),
+]
--- django/week05_hw/accounts/views.py
+++ django/week05_hw/accounts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import AllowAny
+from django.contrib.auth import get_user_model
+from .serializers import UserModelSerializer
 
-# Create your views here.
+User = get_user_model()
+
+
+class UserModelViewSet(ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserModelSerializer
+    permission_classes = [AllowAny]
