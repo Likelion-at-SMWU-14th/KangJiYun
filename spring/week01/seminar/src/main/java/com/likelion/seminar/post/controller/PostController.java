54ff534903aa814fb7b9a50eedbb485004fc39a2
hasol11 <kangji09879@sookmyung.ac.kr>
2026-07-21 20:01:00 +0900

    Feat: Post 조회 기능 구현

--- spring/week01/seminar/src/main/java/com/likelion/seminar/post/controller/PostController.java
+++ spring/week01/seminar/src/main/java/com/likelion/seminar/post/controller/PostController.java
@@ -3,18 +3,36 @@ package com.likelion.seminar.post.controller;
 import com.likelion.seminar.post.dto.PostDTO;
 import com.likelion.seminar.post.service.PostService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/post")
 public class PostController {
     private  final PostService postService;
+
     @PostMapping
     public void createPost(@RequestBody PostDTO postDTO) {
         postService.createPost(postDTO);
+    }
+
+    //게시글 목록 조회
+    @GetMapping
+    public List<PostDTO> getPosts(){
+        return postService.getPosts();
+    }
+
+    //게시글 단일 조회 - /post/1
+    @GetMapping("/{id}")
+    public PostDTO getPostByPathVariable(@PathVariable("id") int id){
+        return postService.getPostById(id);
+    }
+
+    //게시글 단일 조회 - /post/param?id=1
+    @GetMapping("/param")
+    public  PostDTO getPostByRequestParam(@RequestParam("id") int id){
+        return postService.getPostById(id);
     }
 }
