633db58f47d8209e032699f18e65a2f2da3a7e09
hasol11 <kangji09879@sookmyung.ac.kr>
2026-07-21 20:12:53 +0900

    Feat: Post 수정 기능 구현

--- spring/week01/seminar/src/main/java/com/likelion/seminar/post/controller/PostController.java
+++ spring/week01/seminar/src/main/java/com/likelion/seminar/post/controller/PostController.java
@@ -35,4 +35,10 @@ public class PostController {
     public  PostDTO getPostByRequestParam(@RequestParam("id") int id){
         return postService.getPostById(id);
     }
+
+    //게시글 수정
+    @PutMapping("/{id}")
+    public void updatePost(@PathVariable int id, @RequestBody PostDTO postDTO){
+        postService.updatePost(id, postDTO);
+    }
 }
