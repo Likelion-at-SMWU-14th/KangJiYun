c636b9d47c6acca2dbb48aa5268023a6941554fc
hasol11 <kangji09879@sookmyung.ac.kr>
2026-08-11 20:27:34 +0900

    Feat: JPA CRUD 실습

---
+++ spring/week04/seminar/src/main/java/com/example/seminar/entity/Board.java
@@ -0,0 +1,31 @@
+package com.example.seminar.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "board")
+public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @OneToMany(
+            fetch = FetchType.LAZY,
+            mappedBy = "board"
+    )
+    private List<Post> posts = new ArrayList<>();
+
+    public Board(String name){
+        this.name = name;
+    }
+}
